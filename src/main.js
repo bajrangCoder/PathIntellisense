ac35925e15d17b5c30083f37624805fb2df4e9f0
Raunak Raj <nkray21111983@gmail.com>
2023-10-26 06:30:31 +0530

    Patch

--- src/main.js
+++ src/main.js
@@ -45,7 +45,6 @@ class PathIntellisense {
                             callback,
                             false
                         );
-                        return;
                     }
                 }
 
